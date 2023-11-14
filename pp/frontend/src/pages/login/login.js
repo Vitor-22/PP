bd123f9503f6d00245c0bb168fcc562bada5820e
Vitor <vitor.ugo.fiori@gmail.com>
2023-11-14 11:37:04 -0300

    Arrumando o Cadastro

--- pp/frontend/src/pages/login/login.js
+++ pp/frontend/src/pages/login/login.js
@@ -5,10 +5,9 @@ import { useNavigate } from 'react-router-dom';
 import { api } from '../../services/api';
 
 function Login() {
-  document.body.style.overflow = 'hidden'; //  Corta o conteúdo que ultrapassa o tamanho da DIV
 
-  const[user_email, setUserEmail] = useState("");
-  const[user_password, setUserPassword] = useState("");
+  const[email, setEmail] = useState("");
+  const[senha, setSenha] = useState("");
 
   const navigate = useNavigate()
 
@@ -23,11 +22,11 @@ function Login() {
     e.preventDefault();
 
     const data = {
-        user_email,
-        user_password
+        email,
+        senha
     };
+
     try{
-        console.log(data);
         const response = await api.post('/login', data);
 
         console.log(response.data);
@@ -49,24 +48,24 @@ function Login() {
     <>
       <FormContainer>
         
-        <CustomForm onSubmit={handleSubmit}>
+        <CustomForm >
         <Logo src={logoImg}/>
           <CustomLabel>Email</CustomLabel>
           <CustomInput type="text"
-                            id="usuario"
+                            id="email"
                             placeholder="0632454567@senacrs.edu.br"
-                            value={ user_email }
-                            onChange = {(e) => setUserEmail(e.target.value)}
+                            value={ email }
+                            onChange = {(e) => setEmail(e.target.value)}
                             />
           <CustomLabel>Senha</CustomLabel>
           <CustomInput type="password"
                             id="senha"
                             placeholder="Enter your password"
-                            value={ user_password } 
-                            onChange = {(e) => setUserPassword(e.target.value)}
+                            value={ senha } 
+                            onChange = {(e) => setSenha(e.target.value)}
                             />
 
-          <CustomInputSubmit class="submit" id="submit" value="Entrar" type="submit" /> 
+          <CustomInputSubmit onClick={handleSubmit} class="submit" id="submit" value="Entrar" type="submit" /> 
 
           <CustomP>Não possui conta? <CustomLink onClick={goToCadastro}>Cadastre-se</CustomLink></CustomP>
         </CustomForm>
